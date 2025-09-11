5f5cd8d6f595d6a13c1efbf7612997ad40ae84b8
Saravana <67310667+SaravanaNani@users.noreply.github.com>
2025-09-12 00:08:13 +0530

    Create main.tf

---
+++ terraform/modules/ecs-service/main.tf
@@ -0,0 +1,40 @@
+resource "aws_ecs_task_definition" "app" {
+  family                   = "adq-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "adq-container"
+    image     = var.docker_image
+    essential = true
+    portMappings = [{
+      containerPort = var.container_port
+      hostPort      = var.container_port
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "app" {
+  name            = "adq-service"
+  cluster         = var.cluster_id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnets
+    security_groups = [var.sg_id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = var.alb_target_group_arn
+    container_name   = "adq-container"
+    container_port   = var.container_port
+  }
+
+  depends_on = [aws_ecs_task_definition.app]
+}
+
