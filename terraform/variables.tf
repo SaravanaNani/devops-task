2c91ff9b408a3f264c848326e7acbb35778cb5d9
Saravana <67310667+SaravanaNani@users.noreply.github.com>
2025-09-12 10:41:18 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,11 @@
 variable "aws_region" {}
 variable "project" {}
 variable "docker_repo" {}
-variable "image_tag" {}
+variable "image_tag" {
+  description = "Docker image tag for ECS task"
+  default     = "latest"   # any default tag
+}
+
 variable "container_port" {}
 variable "health_path" {}
 variable "cpu" {}
